d2436b00b7df8fdd2fc069f35459cfd76d7b6057
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-12-17 20:26:15 +0100

    yarn add @svg-icons packs, use in Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,17 +1,37 @@
 <script>
   import { stores } from '@sapper/app'
+  import Euro from '@svg-icons/material-sharp/euro.svg'
+  // import Donate from '@svg-icons/fa-solid/donate.svg'
+  // import DonateHeart from '@svg-icons/boxicons-solid/donate-heart.svg'
+  import QaA from '@svg-icons/material-rounded/question-answer.svg'
+  import PeopleCircle from '@svg-icons/ionicons-solid/people-circle.svg'
+  import Rss from '@svg-icons/fa-solid/rss-square.svg'
+  import Contact from '@svg-icons/boxicons-solid/contact.svg'
+  import Place from '@svg-icons/material-filled/place.svg'
+  import Plant from '@svg-icons/remix-fill/plant.svg'
 
   export let nav
 
+  const icons = {
+    Verein: Plant,
+    Standorte: Place,
+    Spenden: Euro,
+    FAQ: QaA,
+    Mitmachen: PeopleCircle,
+    Blog: Rss,
+    Kontakt: Contact,
+  }
   const { page } = stores()
 </script>
 
 <nav>
   {#each nav as { title, url }}
-    <a
-      rel="prefetch"
-      aria-current={url === $page.path ? `page` : undefined}
-      href={url}>{title}</a>
+    <a rel="prefetch" aria-current={url === $page.path ? `page` : undefined} href={url}>
+      <svelte:component
+        this={icons[title]}
+        height="1em"
+        style="vertical-align: -3pt; padding-right: 2pt;" />
+      {title}</a>
   {/each}
 </nav>
 
@@ -20,17 +40,17 @@
     display: contents;
   }
   a {
-    text-decoration: none;
     transition: 0.4s;
-    border-bottom: 3px solid transparent;
-  }
-  a:not(:first-child) {
-    font-weight: lighter;
+    color: var(--headerColor);
   }
   a:hover {
-    color: orange;
+    color: var(--hoverColor);
   }
   a[aria-current] {
-    border-bottom: 3px solid teal;
+    border-radius: 1ex;
+    padding: 3pt 5pt;
+    line-height: 0;
+    background: var(--linkColor);
+    color: white;
   }
 </style>
