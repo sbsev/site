f7a8981a8ab1670df8b6f07d4187189445b8b7cf
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-01-14 09:04:33 +0100

    reinstate lighthouserc assertions

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -7,10 +7,9 @@
 
   const { session } = stores()
   let div, map
-  const mapId = `2c1732cc2265001f` // enables vector maps
 
   // default map props
-  mapProps = { center: { lat: 51.5, lng: 10 }, zoom: 6, mapId, ...mapProps }
+  mapProps = { center: { lat: 51.5, lng: 10 }, zoom: 6, ...mapProps }
 
   const mountMap = () => (map = new window.google.maps.Map(div, mapProps))
 
@@ -18,7 +17,7 @@
     if (!window.google) {
       const script = document.createElement(`script`)
       script.async = true
-      script.src = `https://maps.googleapis.com/maps/api/js?v=beta&key=${$session.GOOGLE_MAPS_API_KEY}&map_ids=${mapId}`
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${$session.GOOGLE_MAPS_API_KEY}`
       document.head.append(script)
       script.addEventListener(`load`, mountMap)
       return () => script.removeEventListener(`load`, mountMap)
