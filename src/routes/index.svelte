518ba68a6512bcf03b802d26c6f18be353eb799c
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-01-16 13:03:29 +0100

    refine landing page tiles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,11 +36,11 @@
   const textBgColors = [`green`, `orange`, `lightBlue`, `darkGreen`, `blue`]
 </script>
 
+<h1>Studenten bilden Schüler e.V.</h1>
 <div class="grid">
-  {#each yaml.images.slice(0, windowWidth > 800 ? 7 : 3) as { src, alt }, idx}
-    <Img {src} {alt} {sizes} pictureStyle="grid-area: img{idx + 1}" />
+  {#each yaml.images.slice(0, windowWidth > 800 ? 9 : windowWidth < 500 ? 3 : 6) as { src, alt }, idx}
+    <Img {src} {alt} {sizes} pictureStyle="grid-area: img{idx + 1};" />
   {/each}
-  <h1 style="grid-area: head;">Studenten bilden Schüler e.V.</h1>
   {#each Object.values(yaml.text) as text, idx}
     <div style="grid-area: txt{idx + 1}; background: var(--{textBgColors[idx]})">
       <span>{text}</span>
@@ -76,38 +76,41 @@
 </article>
 
 <style>
+  h1 {
+    font-size: calc(1em + 2vw);
+  }
   .grid {
-    margin: 2em 3vw;
+    margin: 1em 2vw 2em;
     display: grid;
     grid-gap: 1ex;
-    grid-template-columns: repeat(10, 1fr);
+    font-weight: bolder;
+    grid-template-columns: repeat(11, 1fr);
     grid-template-areas:
-      '. img1 img1 head head head img2 img2 . .'
-      '. img1 img1 . img3 img3 img2 img2 txt1 txt1'
-      'img4 img4 txt2 txt2 img3 img3 img6 img6 img6 .'
-      '. img5 img5 img7 img7 txt3 img6 img6 img6 .';
+      'img8 img8 img8 img3 img3 img3 txt2 txt2 img2 img2 img2'
+      'img4 img4 img4 txt1 txt1 txt1 img9 img9 img6 img6 img6'
+      'img5 img5 img5 img7 img7 img7 txt3 img1 img1 img1 img1';
   }
   @media (max-width: 800px) {
     .grid {
       grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: calc(10em + 6vw);
       grid-template-areas:
-        'head head head'
         'img1 txt1 img2'
-        'txt2 img3 txt3';
+        'txt2 img3 img4'
+        'img5 img6 txt3';
     }
   }
   @media (max-width: 500px) {
     .grid {
       grid-template-columns: 1fr 1fr;
       grid-template-areas:
-        'head head'
         'img1 txt1'
         'txt2 img2'
         'img3 txt3';
     }
   }
   .grid div {
-    border-radius: 1ex;
+    border-radius: 5pt;
     font-size: 2ex;
     display: flex;
     place-items: center;
@@ -116,7 +119,7 @@
     font-size: calc(1em + 0.5vw);
   }
   .grid :global(img) {
-    border-radius: 1ex;
+    border-radius: 5pt;
     height: 100%;
   }
   h2 {
