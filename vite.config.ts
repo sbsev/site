4f5f4bd7d26c66a850719fb17da59458e4cdf3ae
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-07-24 13:31:37 -0700

    address sveltekit breaking changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import rollupYaml from '@rollup/plugin-yaml'
+import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
+import Icons from 'unplugin-icons/vite'
 
 export default {
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [
+    sveltekit(),
+    rollupYaml(),
+    Icons({ compiler: `svelte`, autoInstall: true }),
+  ],
+
+  build: {
+    rollupOptions: {
+      output: { manualChunks: undefined },
+    },
+  },
+
+  resolve: {
+    alias: {
+      $src: path.resolve(`./src`),
+    },
+  },
+
   test: {
     testTimeout: 60_000,
   },
