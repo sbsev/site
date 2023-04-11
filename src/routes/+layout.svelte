7586a9f7d8b0bb7034c628e60dd36d499ccfb858
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-04-11 07:25:44 -0700

    remove duplicated layout.svelte comment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,8 +21,6 @@
   <meta name="author" content={$microcopy?.meta?.name} />
   <meta name="description" content={$microcopy?.meta?.description} />
   <!-- see netlify.toml file for where this script originates -->
-  <!-- see netlify.toml file for where this script originates -->
-  <!-- see netlify.toml file for where this script originates -->
   <script defer data-domain={$microcopy?.meta?.url} src="/js/script.js"></script>
 
   <meta name="color-scheme" content={$colorMode} />
