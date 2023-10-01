c575c6abf5302e86758c98048cae127615a2b3f8
dominikgeissler <39053640+dominikgeissler@users.noreply.github.com>
2023-10-01 17:51:42 +0200

    #115 removed unused type import (#116)
    * #115 removed unused type import
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,8 @@
 import { fetch_chapters, fetch_page } from '$lib/fetch'
-import type { PageServerLoad } from './$types'
 
 export const load = async () => {
   return {
     page: fetch_page(`/`),
     chapters: fetch_chapters(),
-    // ...airtable_fetch(
-    //   `{
-    //     students: studentenStatistiken {
-    //       id
-    //     }
-    //     pupils: schuelerStatistiken {
-    //       id
-    //     }
-    //   }`,
-    //   { cache: `force-cache` }
-    // ),
   }
 }
