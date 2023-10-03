9d7ebe8e1c6bf0034012e79658a82794fab0ca70
root <dominik.geissler@rwth-aachen.de>
2023-10-03 10:58:04 +0200

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,6 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 export default {
   timeout: 100000,
-  expected: {
-    timeout: 10000,
-  },
   webServer: {
     command: `vite dev --port 3005`,
     port: 3005,
