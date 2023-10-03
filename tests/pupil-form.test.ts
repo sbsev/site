9288d19f337efafbff483da532246832371134ec
root <dominik.geissler@rwth-aachen.de>
2023-10-03 11:07:34 +0200

    debug

--- tests/pupil-form.test.ts
+++ tests/pupil-form.test.ts
@@ -1,50 +1,50 @@
-import { expect, test } from '@playwright/test'
-import { fill_place, fill_select, move_slider } from './helpers.ts'
+// import { expect, test } from '@playwright/test'
+// import { fill_place, fill_select, move_slider } from './helpers.ts'
 
-test(`pupil signup form can be submitted after filling all required fields`, async ({
-  page,
-}) => {
-  await page.goto(`/signup-pupil`, { waitUntil: `networkidle` })
+// test(`pupil signup form can be submitted after filling all required fields`, async ({
+//   page,
+// }) => {
+//   await page.goto(`/signup-pupil`, { waitUntil: `networkidle` })
 
-  await fill_select(page, `#chapter`, [`Test`])
+//   await fill_select(page, `#chapter`, [`Test`])
 
-  await fill_select(page, `#gender`, [`Männlich`])
+//   await fill_select(page, `#gender`, [`Männlich`])
 
-  await page.fill(`#firstName`, `Foo Bar`)
+//   await page.fill(`#firstName`, `Foo Bar`)
 
-  await fill_select(page, `input#subjects`, [`Mathe`, `Englisch`])
+//   await fill_select(page, `input#subjects`, [`Mathe`, `Englisch`])
 
-  await fill_select(page, `#schoolTypes`, [`Realschule`])
+//   await fill_select(page, `#schoolTypes`, [`Realschule`])
 
-  await move_slider(page, `.rangeNub`)
+//   await move_slider(page, `.rangeNub`)
 
-  await fill_place(page, `#places input`, `Hamburg`)
-  await page.waitForSelector(`input[data-place='1']`)
+//   await fill_place(page, `#places input`, `Hamburg`)
+//   await page.waitForSelector(`input[data-place='1']`)
 
-  await fill_place(page, `#places input`, `Heidelberg`)
-  await page.waitForSelector(`input[data-place='2']`)
+//   await fill_place(page, `#places input`, `Heidelberg`)
+//   await page.waitForSelector(`input[data-place='2']`)
 
-  await page.fill(`#birthYear`, `2010`)
+//   await page.fill(`#birthYear`, `2010`)
 
-  await page.click(`#online`)
+//   await page.click(`#online`)
 
-  await page.fill(`#nameContact`, `Baz Bar`)
+//   await page.fill(`#nameContact`, `Baz Bar`)
 
-  await page.fill(`#phoneContact`, `012 345 678`)
+//   await page.fill(`#phoneContact`, `012 345 678`)
 
-  await page.fill(
-    `#emailContact`,
-    `florian.kleinicke@studenten-bilden-schueler.de`,
-  )
+//   await page.fill(
+//     `#emailContact`,
+//     `florian.kleinicke@studenten-bilden-schueler.de`,
+//   )
 
-  await page.fill(`#orgContact`, `Privat`)
+//   await page.fill(`#orgContact`, `Privat`)
 
-  await page.click(`#need`)
+//   await page.click(`#need`)
 
-  await page.click(`#dataProtection`)
+//   await page.click(`#dataProtection`)
 
-  await page.click(`button[type=submit].main`)
+//   await page.click(`button[type=submit].main`)
 
-  // make sure we get to the success page
-  expect(await page.locator(`text=🎉 ⭐ 🎉`).textContent()).toBe(`🎉 ⭐ 🎉`)
-})
+//   // make sure we get to the success page
+//   expect(await page.locator(`text=🎉 ⭐ 🎉`).textContent()).toBe(`🎉 ⭐ 🎉`)
+// })
