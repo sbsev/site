8f1741441b04e80af07e53157a3009f3b7626010
dominikgeissler <39053640+dominikgeissler@users.noreply.github.com>
2024-03-03 10:50:04 +0100

    Added security.txt, .gitattributes and PR template (#129)
    * added security.txt, gitattributes
    
    * Added PR template
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -2,10 +2,6 @@ import { fetch_page } from '$lib/fetch'
 import { error } from '@sveltejs/kit'
 
 export const load = async ({ params }) => {
-  // delegate route to static/robots.txt
-  // is this check necessary, will prob fallthrough anyway
-  if (params.slug === `robots.txt`) throw error(404)
-
   const page = await fetch_page(params.slug)
 
   // If no page data could be fetched for params.slug, the page doesn't exist,
